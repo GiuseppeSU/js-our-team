706536e2af7145b65062f01e66c3f16121bf3dfc
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-03-08 16:14:03 +0100

    Finito milestone 0

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const esperti = [
+    {
+        'nome': 'Wayne', 'congnome' : 'Barnett', 'posizione' : 'Founder & CEO'
+    },
+    {
+        'nome': 'Angela', 'congnome' : 'Caroll', 'posizione' : 'Chief Editor'
+    },
+    {
+        'nome': 'Walter', 'congnome' : 'Gordon', 'posizione' : 'Office Manager'
+    },
+    {
+        'nome': 'Angela', 'congnome' : 'Lopez', 'posizione' : 'Social Media Manager'
+    },
+    {
+        'nome': 'Scott', 'congnome' : 'Estrada', 'posizione' : 'Developer'
+    },
+
+    {
+        'nome': 'Barbara', 'congnome' : 'Ramos', 'posizione' : 'Graphic Designer'
+    },
+]
